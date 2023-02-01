28d7beec66d45d0ce044a4d9e55b171cac8b9f25
Tarun <tarunsmtgroup@gmail.com>
2023-02-01 12:28:15 +0530

    Initial commit

---
+++ template/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): JSX.Element {
+
+
+
+  return (<View>
+   <Text>Test app</Text>
+   </View>
+  );
+}
+
+export default App;
